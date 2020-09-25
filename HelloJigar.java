f980a38ea06280b33a415e35aa833c6b81fcf9be
Jigar Bhatt <60162196+jigar-lab@users.noreply.github.com>
2020-09-25 15:47:12 -0700

    Update HelloJigar.java

--- HelloJigar.java
+++ HelloJigar.java
@@ -6,7 +6,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, Jigar"); 
-               System.out.println("Hello, Jigar1"); 
+        System.out.println("Hello, Jigar");
     } 
 } 
